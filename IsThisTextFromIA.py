283d981aca0f34de4a9c0697acfaf43cc2220161
Killian Cengo <cengokillian@hotmail.com>
2022-12-08 22:51:16 +0100

    Ajout de fonctionnalités de mémorisation des textes incorrectement détectés à l'apprentissage.

--- IsThisTextFromIA.py
+++ IsThisTextFromIA.py
@@ -7,24 +7,66 @@ from nltk.corpus import stopwords # module qui contient une liste de mots couran
 MOTS_COURANTS = "mots_courants.txt"
 PATH_HUMAN_TEXTS = "/humain/"
 PATH_AI_TEXTS = "/ia/"
+CURRENTPATH = os.getcwd()
+
+# Fonction qui recherche une chaine dans un texte en utilisant l'algorithme de Karp-Rabin
+def recherche_KarpRabin(text, pattern):
+    pattern_hash = hash(pattern)  # calculer le hachage de la chaîne à rechercher
+    for i in range(len(text) - len(pattern) + 1):
+        # calculer le hachage de la sous-chaîne de longueur égale à celle de la chaîne à rechercher
+        sub_string_hash = hash(text[i:i + len(pattern)])
+        if pattern_hash == sub_string_hash:  # si les hachages sont égaux, vérifier si les chaînes sont égales
+            if text[i:i + len(pattern)] == pattern:
+                return True  # retourner True si les chaînes sont égales
+    return False  # retourner False si la chaîne n'a pas été trouvée
+
+# Recherche un texte dans plusieurs fichiers
+def recherche_chaine_dossier(text, dossier):
+    for filename in os.listdir(dossier):
+        if filename.endswith(".txt"):
+            if recherche_KarpRabin(read_one_text(dossier + filename), text):
+                return True
+    return False
+def save_text(new_text, path):
+    print("Enregistrement du texte...")
+    if len(new_text) < 1000:
+        # On ouvre le fichier train.txt qui contient le numéro le plus élevé
+        num = 0
+        for filename in os.listdir(path):
+            if filename.startswith("train"):
+                num = int(filename[5])
+        with open(path + "train" + str(num) + ".txt", "a",
+                  encoding="utf-8") as f:  # l'ouverture en "a" permet d'ajouter du texte à la fin du fichier
+            # on ajoute retour à la ligne et le texte
+            f.write("\n" + new_text)
+        f.close()
+    else:
+        # On crée un nouveau fichier train.txt
+        num = 0
+        for filename in os.listdir(path):
+            if filename.startswith("train"):
+                num = int(filename[5])
+        with open(path + "train" + str(num + 1) + ".txt", "w", encoding="utf-8") as f:
+            f.write(new_text)
+        f.close()
+    print("Merci! Le fichier a été enregistré.")
 
 # Fonction qui lit plusieurs fichiers texte dans PATH et les convertis une chaine de caractères
 def read_texts(a):
     chaine = []
     # chemin courant
-    path = os.getcwd()
+    chemin = CURRENTPATH
     if a==1 :
-        path = path+PATH_HUMAN_TEXTS
+        chemin = chemin+PATH_HUMAN_TEXTS
     else :
-        path = path+PATH_AI_TEXTS
-    for filename in os.listdir(path):
+        chemin = chemin+PATH_AI_TEXTS
+    for filename in os.listdir(chemin):
         if filename.endswith(".txt"):
-            chaine.append(read_one_text(path + filename))
+            chaine.append(read_one_text(chemin + filename))
     return chaine
 
 # Fonction qui lit un fichier texte et le convertis en une chaine de caractères
 def read_one_text(filename):
-    print("lecture du fichier " + filename + "...")
     texts = []
     with open(filename, "r", encoding='utf-8') as file:
         for line in file:
@@ -80,62 +122,100 @@ def rare_word_use(text):
         return 0
     return num_rare_words / num_words
 
+# ENTRAINEMENT
+# Fonction qui entraine le modèle
+def train_model():
+    # Charger les textes d'entraînement
+    texts_from_ai = read_texts(0)
+    texts_from_human = read_texts(1)
+
+    # Ecrire dans un fichier les mots courants
+    write_common_words(MOTS_COURANTS)
+
+    # Définir les labels correspondants pour chaque texte (0 pour IA, 1 pour humain)
+    # labels doit contenir autant d'éléments que de textes d'entraînement (len(texts_from_ai) + len(texts_from_human))
+    labels = [0] * len(texts_from_ai) + [1] * len(texts_from_human)
+
+    # Extraire les caractéristiques des textes (nombre de mots, longueur moyenne des phrases, et nombre de mots uniques, répétition des mots)
+    num_words = []
+    avg_sentence_length = []
+    num_unique_words = []
+    num_repeat_words = []
+    num_rare_word_use = []
+    print("Entraînement du modèle...")
+    for texts in [texts_from_ai, texts_from_human]:
+        num_words.extend(
+            [count_words(text) for text in texts])  # .extend permet d'ajouter les éléments d'une liste à une autre
+        avg_sentence_length.extend([average_sentence_length(text) for text in texts])
+        num_unique_words.extend([count_unique_words(text) for text in texts])
+        num_repeat_words.extend([count_repeated_words(text) for text in texts])
+        num_rare_word_use.extend([rare_word_use(text) for text in texts])
+
+    # Créer un modèle de régression logistique
+    model = LogisticRegression()
+
+    # Entraîner le modèle en utilisant les textes et les labels
+    model.fit(np.column_stack([num_words, avg_sentence_length, num_unique_words, num_repeat_words, num_rare_word_use]),
+              labels)
+    # .fit permet d'entraîner le modèle en utilisant les données d'entraînement et les labels correspondants (les labels sont les bonnes réponses)
+    return model
+
 # PROGRAMME PRINCIPAL
+if __name__ == "__main__":
+    # Entrainer le modèle
+    model = train_model()
 
-# Charger les textes d'entraînement
-texts_from_ai = read_texts(0)
-texts_from_human = read_texts(1)
+    # Prédire si le contenu entier d'un fichier texte provient d'une IA ou d'un humain
+    new_text = read_one_text("new_text.txt")
+    num_words = count_words(new_text)
+    avg_sentence_length = average_sentence_length(new_text)
+    num_unique_words = count_unique_words(new_text)
+    num_repeat_words = count_repeated_words(new_text)
+    num_rare_word_use = rare_word_use(new_text)
+    # Il faut que les données d'entraînement et les données à prédire aient le même nombre de dimensions
+    # Ici, les données d'entraînement sont des tableaux à deux dimensions et les données à prédire sont des tableaux à une dimension
+    # Pour transformer un tableau à une caractéristique en tableau à trois caractéristiques, on utilise la fonction column_stack
+    print("Prédiction: ")
+    X = np.column_stack([num_words, avg_sentence_length, num_unique_words, num_repeat_words, num_rare_word_use])
+    prediction = model.predict(X)
+    # donne la prédiction avec sa probabilité
+    probabilities = model.predict_proba(X)
+    proba_arrondi_IA = round(probabilities[0][0]*100, 2)
+    proba_arrondi_humain = round(probabilities[0][1]*100, 2)
 
-# Ecrire dans un fichier les mots courants
-write_common_words(MOTS_COURANTS)
+    textFromIA = 0
+    if prediction == 0:
+        print("Ce texte a été généré par une IA avec une probabilité de", proba_arrondi_IA, "%")
+        textFromIA = 1
+    else:
+        print("Ce texte a été écrit par un humain avec une probabilité de", proba_arrondi_humain, "%")
+        textFromIA = 0
 
-# Définir les labels correspondants pour chaque texte (0 pour IA, 1 pour humain)
-# labels doit contenir autant d'éléments que de textes d'entraînement (len(texts_from_ai) + len(texts_from_human))
-labels = [0] * len(texts_from_ai) + [1] * len(texts_from_human)
+    # Demande à l'utilisateur si c'est correct
+    print("Est-ce correct? (O/N)")
+    correct = input()
+    if correct == "O":
+        print("Merci!")
+    else:
+        if textFromIA == 1:
+            path = CURRENTPATH + PATH_HUMAN_TEXTS
+        else:
+            path = CURRENTPATH + PATH_AI_TEXTS
+        # On recherche le texte new_text.txt en utilisant la fonction search(text, pattern) dans tous les fichiers textes d'entraînement situés dans path
+        # Si le texte est trouvé, on arrête le programme. Sinon, on enregistre le texte dans un nouveau fichier
+        if recherche_chaine_dossier(new_text, path):
+            print("Ce texte a déjà été enregistré")
+        else:
+            # On crée un fichier qui sera enregistré dans PATH_HUMAN_TEXTS si textFromIA = 1 et dans PATH_AI_TEXTS si textFromIA = 0
+            save_text(new_text, path)
+    #On vide le contenu de new_text.txt
+    with open("new_text.txt", "w") as f:
+        f.write("")
+        f.close()
 
-# Extraire les caractéristiques des textes (nombre de mots, longueur moyenne des phrases, et nombre de mots uniques, répétition des mots)
-num_words = []
-avg_sentence_length = []
-num_unique_words = []
-num_repeat_words = []
-num_rare_word_use = []
-print("Entraînement du modèle...")
-for texts in [texts_from_ai, texts_from_human]:
-    num_words.extend([count_words(text) for text in texts])#.extend permet d'ajouter les éléments d'une liste à une autre
-    avg_sentence_length.extend([average_sentence_length(text) for text in texts])
-    num_unique_words.extend([count_unique_words(text) for text in texts])
-    num_repeat_words.extend([count_repeated_words(text) for text in texts])
-    num_rare_word_use.extend([rare_word_use(text) for text in texts])
 
-# Créer un modèle de régression logistique
-model = LogisticRegression()
 
-# Entraîner le modèle en utilisant les textes et les labels
-model.fit(np.column_stack([num_words, avg_sentence_length, num_unique_words, num_repeat_words, num_rare_word_use]), labels)
-# .fit permet d'entraîner le modèle en utilisant les données d'entraînement et les labels correspondants (les labels sont les bonnes réponses)
 
-# Prédire si le contenu entier d'un fichier texte provient d'une IA ou d'un humain
-new_text = read_one_text("new_text.txt")
-num_words = count_words(new_text)
-avg_sentence_length = average_sentence_length(new_text)
-num_unique_words = count_unique_words(new_text)
-num_repeat_words = count_repeated_words(new_text)
-num_rare_word_use = rare_word_use(new_text)
-# Il faut que les données d'entraînement et les données à prédire aient le même nombre de dimensions
-# Ici, les données d'entraînement sont des tableaux à deux dimensions et les données à prédire sont des tableaux à une dimension
-# Pour transformer un tableau à une caractéristique en tableau à trois caractéristiques, on utilise la fonction column_stack
-print("Prédiction: ")
-X = np.column_stack([num_words, avg_sentence_length, num_unique_words, num_repeat_words, num_rare_word_use])
-prediction = model.predict(X)
-# donne la prédiction avec sa probabilité
-probabilities = model.predict_proba(X)
-proba_arrondi_IA = round(probabilities[0][0], 4)
-proba_arrondi_humain = round(probabilities[0][1], 4)
-
-if prediction == 0:
-    print("Ce texte a été généré par une IA avec une probabilité de", proba_arrondi_IA * 100, "%")
-else:
-    print("Ce texte a été écrit par un humain avec une probabilité de", proba_arrondi_humain * 100, "%")
 
 
 
